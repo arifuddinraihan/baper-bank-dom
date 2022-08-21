649fa5dda9dc41489f7870b56187902211df109f
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-08-21 14:20:23 +0600

    error fixed in deposit js

--- js/deposit.js
+++ js/deposit.js
@@ -2,16 +2,23 @@
 
 document.getElementById('btn-deposit').addEventListener('click', function(){
     // step 2: get the input data from input field using a variable
-    const depositInputValue = document.getElementById('deposit-amount');
     // step 2.a: get the value of input data
-    const depositValue = parseFloat(depositInputValue.value).toFixed(2);
-    // step 3: Get the deposit number view-data using an id
-    const depositView = document.getElementById("deposit-view");
-    // step 3.a: get the inner data of that deposit using .innerText 
-    const depositViewAmount = parseFloat(depositView.innerText).toFixed(2);
+    const depositInputValue = document.getElementById('deposit-amount');
+    const newDepositValue = parseFloat(depositInputValue.value);
+    // console.log(newDepositValue);
     
-    // step 4: swap the input field with deposti value that you inputed
-    depositView.innerText = depositValue;
+    // step 3: Get the deposit number view-data using an id
+    // step 3.a: get the inner data of that deposit using .innerText
+    const depositView = document.getElementById('deposit-view'); 
+    const previousDepositAmount = parseFloat(depositView.innerText);
+    // console.log(previousDepositAmount);
+    
+    // step 4: Sum the previous deposit with new deposti amount
+    const currentDepositAmount = newDepositValue + previousDepositAmount;
+    console.log(currentDepositAmount);
+
+    // step 5: swap the innertext of the deposit view with new deposit ammount
+    depositView.innerText = currentDepositAmount;
     
     // step 7: clear the deposit input field
     depositInputValue.value = '';
